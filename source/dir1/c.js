e082635796e1744a8d2ed1f6b9f7a01bd143ad86
root <root@DESKTOP-3UTPEGN.localdomain>
2020-12-24 15:06:27 +0900

    modify

---
+++ source/dir1/c.js
@@ -0,0 +1,10 @@
+// 日本語
+(new function() {
+    console.log("c#c");
+    var self = this;
+    self.start = function() {
+        console.log("c#start");
+        var me = this;
+    }
+    self.start();
+}());
